59c12394a6d08e4298c66c59b865c9b72c3efad7
meanderson <meanderson@10.255.81.8>
2019-03-19 18:59:26 -0400

    Jake doesn't know what to do, he sucks

--- src/javaapplication/JavaApplication.java
+++ src/javaapplication/JavaApplication.java
@@ -17,6 +17,7 @@ public class JavaApplication {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new NewJFrame().setVisible(true);
     }
     
 }
